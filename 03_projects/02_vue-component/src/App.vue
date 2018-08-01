485b5511df6fc2c18fd7c067a770a5a1a191740b
jason <jason@jasondeMac-mini.local>
2018-08-01 12:21:37 +0800

    project-vue-component

---
+++ 03_projects/02_vue-component/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="main">
+        {{msg}}
+        <my-component name="Jason"/>
+    </div>
+</template>
+<script>
+    import MyComponent from './components/MyComponent';
+    export  default  {
+        data(){
+            return {
+                msg: 'this is app.vue'
+            }
+        },
+        components: {
+            MyComponent
+        }
+    }
+</script>
+<style scoped>
+    .main {
+        width: 100%;
+        height: 100%;
+        padding-top: 200px;
+        text-align: center;
+        font-size: 24px;
+        color: #f33;
+    }
+</style>
---
+++ 03_projects/02_vue-component/src/components/MyComponent.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        {{message}} {{name}}
+    </div>
+</template>
+
+<script>
+    export  default  {
+        data(){
+            return {
+                message: 'Hello Component'
+            }
+        },
+        props:['name']
+    }
+</script>
